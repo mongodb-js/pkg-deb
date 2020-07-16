74dfc49c9c175023f84a9f3fb12ae8e26b6fb11e
Irina Shestak <shestak.irina@gmail.com>
2020-07-16 15:39:00 +0200

    set up an instance of debian installer as standalon

---
+++ src/read-metadata.js
@@ -0,0 +1,22 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+/**
+ * Read `package.json` either from `$RESOURCES_DIR/app.asar` (if the app is packaged)
+ * or from `$RESOURCES_DIR/app/package.json` (if it is not). `$RESOURCES_DIR` is either
+ * `AppName.app/Contents/Resources` on macOS, or `resources` on other platforms.
+ *
+ * Options used:
+ *
+ * * `src`: the directory containing the bundled app
+ * * `logger`: function that handles debug messages, e.g.,
+ *             `debug('electron-installer-something:some-module')`
+ */
+module.exports = async function readMetadata (options) {
+  const appPackageJSONPath = path.join(options.sourceDir, 'package.json')
+  options.logger(`Reading package metadata from ${appPackageJSONPath}`)
+  return fs.readJson(appPackageJSONPath)
+    .catch( function (err) {
+      throw new Error(`Could not find, read, or parse package.json '${options.sourceDir}':\n${err.message}`)
+    })
+}
