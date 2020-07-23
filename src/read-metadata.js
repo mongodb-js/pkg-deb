0da99177f36db8ce0b4d771045fafd5a6a32d2bc
Irina Shestak <shestak.irina@gmail.com>
2020-07-23 11:38:47 +0200

    cleanup new API for PackageDebian

--- src/read-metadata.js
+++ src/read-metadata.js
@@ -2,21 +2,19 @@ const fs = require('fs-extra')
 const path = require('path')
 
 /**
- * Read `package.json` either from `$RESOURCES_DIR/app.asar` (if the app is packaged)
- * or from `$RESOURCES_DIR/app/package.json` (if it is not). `$RESOURCES_DIR` is either
- * `AppName.app/Contents/Resources` on macOS, or `resources` on other platforms.
+ * Find and read package.json from src dir to construct metadata necessary for
+ * control file.
  *
  * Options used:
  *
  * * `src`: the directory containing the bundled app
- * * `logger`: function that handles debug messages, e.g.,
- *             `debug('electron-installer-something:some-module')`
+ * * `logger`: function that handles debug messages
  */
 module.exports = async function readMetadata (options) {
-  const appPackageJSONPath = path.join(options.input, '..', '..', 'package.json')
-  console.log(`Reading package metadata from ${appPackageJSONPath}`)
+  const appPackageJSONPath = path.join(options.src, 'package.json')
+  options.logger(`Reading package metadata from ${appPackageJSONPath}`)
   return fs.readJson(appPackageJSONPath)
     .catch( function (err) {
-      throw new Error(`Could not find, read, or parse package.json '${options.input}':\n${err.message}`)
+      throw new Error(`Could not find, read, or parse package.json '${options.src}':\n${err.message}`)
     })
 }
