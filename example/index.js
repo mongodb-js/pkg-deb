16fef73ecf30d1771267b3b9e3bd8470f13f0a3a
Irina Shestak <shestak.irina@gmail.com>
2020-08-07 17:02:14 +0200

    add example use case in example dir

---
+++ example/index.js
@@ -0,0 +1,14 @@
+const pkgDeb = require('../')
+const path = require('path')
+
+const opts = {
+  version: '0.0.1',
+  name: 'lilbins',
+  dest: path.join(__dirname, 'dist'),
+  src: path.join(__dirname),
+  input: path.join(__dirname, 'lilbins'),
+  arch: 'amd64',
+  logger: console.log
+}
+
+pkgDeb(opts).catch(e => console.log(e))
